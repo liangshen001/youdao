7bd297c44fb2a3a1540f10acafb49268dd1418fb
liangshen001 <wangliang520wl@hotmail.com>
2023-02-18 17:44:22 +0800

    init

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -2,11 +2,11 @@ import Youdao from "../src";
 
 describe("Test", () => {
     test("index", () => {
-        const youdao = new Youdao({
-            appKey: '0465a5910712d77c',
-            appSecret: 'ILuULehP522xSNX1fNbUaJbv5y53AQdY',
-        });
-        youdao.translate('unknown primary or operator')
-            .then(a => console.log(a));
+        // const youdao = new Youdao({
+        //     appKey: '0465a5910712d77c',
+        //     appSecret: 'ILuULehP522xSNX1fNbUaJbv5y53AQdY',
+        // });
+        // youdao.translate('unknown primary or operator')
+        //     .then(a => console.log(a));
     });
 });
